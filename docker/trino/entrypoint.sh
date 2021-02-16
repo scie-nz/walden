5522cbb6c85ea74a9b81c17bd7cb6d8c96496068
Bogdan State <bogdan@scie.nz>
2021-02-16 17:58:29 +1300

    Added Trino config + Docker image

---
+++ docker/trino/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash -c "echo -e \"$(cat etc/node.properties.template)\" > etc/node.properties"
+bash -c "echo -e \"$(cat etc/config.properties.template)\" > etc/config.properties"
+bin/launcher run
---
+++ docker/trino/push.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker push scienz/walden-trino
