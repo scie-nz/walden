6d0f85105430f13d045f54651247f3fcb3e3083c
Bogdan State <bogdan@scie.nz>
2022-01-19 22:29:18 +1300

    Updated trino to 368

--- docker/trino/entrypoint.sh
+++ docker/trino/entrypoint.sh
@@ -2,4 +2,4 @@
 
 bash -c "echo -e \"$(cat etc/node.properties.template)\" > etc/node.properties"
 bash -c "echo -e \"$(cat etc/config.properties.template)\" > etc/config.properties"
-bin/launcher run
+bin/launcher run -v
