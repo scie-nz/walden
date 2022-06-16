db838f3ef4375a09ad24b0396908fc632a6e78de
Nick Parker <nick@scie.nz>
2022-06-16 19:47:13 +1200

    Switch from /walden to /ro when setting up configs

--- kube/configs/superset_copy_configs.sh
+++ kube/configs/superset_copy_configs.sh
@@ -4,24 +4,24 @@
 # Broken out into a standalone script file to be executed by both superset and superset-worker on startup.
 
 # Expected volumes/mounts:
-# - superset-config ConfigMap to /walden/
+# - superset-config ConfigMap to /ro/
 # - superset-config-custom ConfigMap to /custom/
 # - EmptyDir to /out/
 
 # BEFORE:
-#   /walden/superset_copy_configs.sh (this script)
-#   /walden/superset_config.py (default walden config)
+#   /ro/superset_copy_configs.sh (this script)
+#   /ro/superset_config.py (default config)
 #   /custom/superset_config.py (user config to append, if present)
 #   /custom/* (other user files)
 #   /secrets/* (any user secrets)
 # AFTER:
 #   /out/superset_config.py (default + user configs, appended)
-#   /out/* (all walden+user files, with user files taking precedence)
+#   /out/* (all provided+user files, with user files taking precedence)
 
-echo "Copying default configs/scripts from /walden"
-cp -aLv /walden/* /out/
+echo "Copying default configs/scripts from /ro"
+cp -aLv /ro/* /out/
 
-# Users may completely override walden configs (except for superset_config.py),
+# Users may completely override ro configs (except for superset_config.py),
 # or can just make additive changes via e.g. the superset_init_custom.sh hook.
 echo "Copying custom configs/scripts from /custom (superset-custom ConfigMap)"
 cp -aLv /custom/* /out/
@@ -32,8 +32,8 @@ cp -aLv /secrets/* /out/
 
 # Special case for superset_config.py: Concatenate the two files
 if [ -f "/custom/superset_config.py" ]; then
-    echo "Using concatenated /walden/superset_config.py + /custom/superset_config.py"
-    cat /walden/superset_config.py /custom/superset_config.py > /out/superset_config.py
+    echo "Using concatenated /ro/superset_config.py + /custom/superset_config.py"
+    cat /ro/superset_config.py /custom/superset_config.py > /out/superset_config.py
 fi
 
 echo "Superset config:"
